103dcd177098c30bfa13b326a1076803c3bbf04d
MyMiDiII <mymidi.student@yandex.ru>
2023-05-02 12:53:34 +0300

    ADD: lindex base

---
+++ src/lindex/lindex.py
@@ -0,0 +1,15 @@
+class Lindex:
+    def __init__(self, keys: list[int], data: list[Any]):
+        pass
+
+    def _build_model(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self, key: int) -> int:
+        pass
+
+    def predict_range(self, low, hight) -> tuple[int, int]:
+        pass
