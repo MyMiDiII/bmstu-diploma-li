e47aa409c83605f1f03b07093d2921ae4bc69b18
MyMiDiII <mymidi.student@yandex.ru>
2023-05-02 14:41:11 +0300

    ADD: lindex based on simple neural network

--- src/lindex/lindex.py
+++ src/lindex/lindex.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import tensorflow as tf
+
 
 class Lindex:
     def __init__(self, keys: list[int], data: list[any]):
@@ -9,22 +11,47 @@ class Lindex:
         self.data = np.array(data)[sort_indexes]
         self.positions = np.arange(0, len(self.keys))
 
+        # print(f"true pos\n {np.where(self.keys == keys[0])}")
+        # print(f"bkeys\n {keys}")
+        # print(f"bdata\n {data}")
+        # print(f"keys\n {self.keys}")
+        # print(f"data\n {self.data}")
+        # print(f"poss\n {self.positions}")
+
+        self.model = self._build_model()
+        self.model.summary()
+
     def _build_model(self):
-        pass
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(128, activation="relu", input_dim=1),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(1, activation="linear")
+            ])
+        model.compile(loss='mean_squared_error')
+
+        return model
 
     def train(self):
-        pass
+        self.model.fit(self.keys, self.positions, epochs=500, verbose=0)
 
     def predict(self, key: int) -> int:
-        pass
+        predicted_position = self.model.predict(np.array([[key]]))
+        print(predicted_position)
 
     def predict_range(self, low, hight) -> tuple[int, int]:
         pass
 
 
 if __name__ == "__main__":
-    size = 3
+    np.random.seed(0)
+
+    size = 1000
     keys = np.random.uniform(0, 10000, size).astype(int)
     values = np.random.randint(0, 1000, size)
 
+    key = keys[0]
     index = Lindex(keys, values)
+    index.train()
+    index.predict(key)
+
+    print(f"pkey {key}")
