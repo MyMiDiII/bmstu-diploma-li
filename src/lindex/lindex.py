972cffd05f4cee143b9b02af9c0ac4735fd26f87
MyMiDiII <mymidi.student@yandex.ru>
2023-05-02 13:58:18 +0300

    ADD: lindex data preparation

--- src/lindex/lindex.py
+++ src/lindex/lindex.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 class Lindex:
     def __init__(self, keys: list[int], data: list[any]):
-        pass
+        sort_indexes = np.argsort(keys)
+
+        self.keys = np.array(keys)[sort_indexes]
+        self.data = np.array(data)[sort_indexes]
+        self.positions = np.arange(0, len(self.keys))
 
     def _build_model(self):
         pass
@@ -13,3 +20,11 @@ class Lindex:
 
     def predict_range(self, low, hight) -> tuple[int, int]:
         pass
+
+
+if __name__ == "__main__":
+    size = 3
+    keys = np.random.uniform(0, 10000, size).astype(int)
+    values = np.random.randint(0, 1000, size)
+
+    index = Lindex(keys, values)
