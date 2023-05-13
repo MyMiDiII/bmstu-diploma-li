59f4d18e46f68b99ef88a54309b60f59a92f4f2e
MyMiDiII <mymidi.student@yandex.ru>
2023-05-13 12:25:01 +0300

    ADD: not eponymous virtual table with makefile

--- src/sqlite/src/mytable.c
+++ src/sqlite/src/mytable.c
@@ -49,6 +49,7 @@
 SQLITE_EXTENSION_INIT1
 #include <string.h>
 #include <assert.h>
+#include <stdio.h>
 
 /* templatevtab_vtab is a subclass of sqlite3_vtab which is
 ** underlying representation of the virtual table
@@ -84,13 +85,14 @@ struct mytable_cursor {
 **    (2) Tell SQLite (via the sqlite3_declare_vtab() interface) what the
 **        result set of queries against the virtual table will look like.
 */
-static int mytableConnect(
+static int mytableCreate(
   sqlite3 *db,
   void *pAux,
   int argc, const char *const*argv,
   sqlite3_vtab **ppVtab,
   char **pzErr
 ){
+    puts("CREATE");
   mytable_vtab *pNew;
   int rc;
 
@@ -109,10 +111,22 @@ static int mytableConnect(
   return rc;
 }
 
+static int mytableConnect(
+  sqlite3 *db,
+  void *pAux,
+  int argc, const char *const*argv,
+  sqlite3_vtab **ppVtab,
+  char **pzErr
+){
+    puts("CONNECT");
+    return mytableCreate(db, pAux, argc, argv, ppVtab, pzErr);
+}
+
 /*
 ** This method is the destructor for templatevtab_vtab objects.
 */
 static int mytableDisconnect(sqlite3_vtab *pVtab){
+    puts("DISCONNECT");
   mytable_vtab *p = (mytable_vtab*)pVtab;
   sqlite3_free(p);
   return SQLITE_OK;
@@ -122,6 +136,7 @@ static int mytableDisconnect(sqlite3_vtab *pVtab){
 ** Constructor for a new templatevtab_cursor object.
 */
 static int mytableOpen(sqlite3_vtab *p, sqlite3_vtab_cursor **ppCursor){
+    puts("OPEN");
   mytable_cursor *pCur;
   pCur = sqlite3_malloc( sizeof(*pCur) );
   if( pCur==0 ) return SQLITE_NOMEM;
@@ -134,6 +149,7 @@ static int mytableOpen(sqlite3_vtab *p, sqlite3_vtab_cursor **ppCursor){
 ** Destructor for a templatevtab_cursor.
 */
 static int mytableClose(sqlite3_vtab_cursor *cur){
+    puts("CLOSE");
   mytable_cursor *pCur = (mytable_cursor*)cur;
   sqlite3_free(pCur);
   return SQLITE_OK;
@@ -144,6 +160,7 @@ static int mytableClose(sqlite3_vtab_cursor *cur){
 ** Advance a templatevtab_cursor to its next row of output.
 */
 static int mytableNext(sqlite3_vtab_cursor *cur){
+    puts("NEXT");
   mytable_cursor *pCur = (mytable_cursor*)cur;
   pCur->iRowid++;
   return SQLITE_OK;
@@ -158,6 +175,7 @@ static int mytableColumn(
   sqlite3_context *ctx,       /* First argument to sqlite3_result_...() */
   int i                       /* Which column to return */
 ){
+  puts("COLUMN");
   mytable_cursor *pCur = (mytable_cursor*)cur;
   switch( i ){
     case MYTABLE_A:
@@ -176,6 +194,7 @@ static int mytableColumn(
 ** rowid is the same as the output value.
 */
 static int mytableRowid(sqlite3_vtab_cursor *cur, sqlite_int64 *pRowid){
+    puts("ROWID");
   mytable_cursor *pCur = (mytable_cursor*)cur;
   *pRowid = pCur->iRowid;
   return SQLITE_OK;
@@ -186,6 +205,7 @@ static int mytableRowid(sqlite3_vtab_cursor *cur, sqlite_int64 *pRowid){
 ** row of output.
 */
 static int mytableEof(sqlite3_vtab_cursor *cur){
+    puts("EOF");
   mytable_cursor *pCur = (mytable_cursor*)cur;
   return pCur->iRowid>=10;
 }
@@ -201,6 +221,7 @@ static int mytableFilter(
   int idxNum, const char *idxStr,
   int argc, sqlite3_value **argv
 ){
+    puts("FILTER");
   mytable_cursor *pCur = (mytable_cursor *)pVtabCursor;
   pCur->iRowid = 1;
   return SQLITE_OK;
@@ -216,6 +237,7 @@ static int mytableBestIndex(
   sqlite3_vtab *tab,
   sqlite3_index_info *pIdxInfo
 ){
+    puts("BEST");
   pIdxInfo->estimatedCost = (double)10;
   pIdxInfo->estimatedRows = 10;
   return SQLITE_OK;
@@ -227,11 +249,11 @@ static int mytableBestIndex(
 */
 static sqlite3_module mytableModule = {
   /* iVersion    */ 0,
-  /* xCreate     */ 0,
+  /* xCreate     */ mytableCreate,
   /* xConnect    */ mytableConnect,
   /* xBestIndex  */ mytableBestIndex,
   /* xDisconnect */ mytableDisconnect,
-  /* xDestroy    */ 0,
+  /* xDestroy    */ mytableDisconnect,
   /* xOpen       */ mytableOpen,
   /* xClose      */ mytableClose,
   /* xFilter     */ mytableFilter,
@@ -264,5 +286,6 @@ int sqlite3_mytable_init(
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
   rc = sqlite3_create_module(db, "mytable", &mytableModule, 0);
+  printf("rc = %d\n", rc);
   return rc;
 }
