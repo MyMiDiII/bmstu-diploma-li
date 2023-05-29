e157eab30f9fea7f318279ae76676867661f3617
MyMiDiII <mymidi.student@yandex.ru>
2023-05-29 20:23:11 +0300

    ADD: index range predict

--- src/indexes/lindex.py
+++ src/indexes/lindex.py
@@ -11,6 +11,7 @@ from indexes.models.abs_model import AbstractModel
 
 from utils.timer import timer
 
+#from line_profiler import LineProfiler
 
 class Lindex:
     def __init__(self, model: AbstractModel):
@@ -93,16 +94,24 @@ class Lindex:
 
         vec_clarify = np.vectorize(clarify_one)
         positions = vec_clarify(keys, positions)
+        #print("POS ALL", positions)
         return positions[positions >= 0]
 
     @timer
+    #@profile
     def find(self, keys):
         if not self.trained or not keys:
             return None
 
         keys = np.array(keys)
+        #print("SELF KEYS", self.keys)
         positions = self._predict(keys)
+        #print("POS NOT CLARIFY", positions)
         positions = self._clarify(keys, positions)
+
+        #print("POSITIONS", positions)
+        #print("DATA", self.data[positions])
+
         return self.data[positions]
 
     def insert(self, key, data):
@@ -113,8 +122,79 @@ class Lindex:
 
         self._true_train()
 
-    def predict_range(self, low, hight) -> tuple[int, int]:
-        pass
+    def _range_binsearch(self, key, lower, upper):
+        while lower <= upper:
+            mid = (lower + upper) // 2
+            if self.keys[mid] == key:
+                return mid, mid
+            elif self.keys[mid] < key:
+                lower = mid + 1
+            else:
+                upper = mid - 1
+
+        return min(lower, upper), max(lower, upper)
+
+    def _range_clarify(self, key, limit, is_lower, constraint):
+        if is_lower and np.isnan(limit):
+            return 0
+        elif np.isnan(limit):
+            return self.N - 1
+
+        key = int(key)
+        limit = int(limit)
+
+        limit = max(min(limit, self.N - 1), 0)
+
+        lower, upper = limit, limit
+
+        if self.keys[limit] != key:
+            bin_lower = max(limit - self.mean_abs_err, 0)
+            bin_upper = min(limit + self.mean_abs_err, self.N - 1)
+
+            if not (self.keys[bin_lower] < key < self.keys[bin_upper]):
+                bin_lower = max(limit - self.max_abs_err, 0)
+                bin_upper = min(limit + self.max_abs_err, self.N - 1)
+
+            lower, upper = self._range_binsearch(key, bin_lower, bin_upper)
+
+        #print("_range_clarify")
+        #print(lower, upper)
+
+        if lower == upper:
+            #print(is_lower)
+            #print(int(is_lower))
+            #print(constraint)
+            #print((-1) ** int(is_lower) * constraint)
+            return lower - (-1) ** int(is_lower) * constraint
+
+        if is_lower:
+            return upper
+
+        return lower
+
+    @timer
+    def predict_range(self, keys_range, constraint):
+        if keys_range[0] is not None and keys_range[1] is not None:
+            if keys_range[0] > keys_range[1]:
+                keys_range[0], keys_range[1] = keys_range[1], keys_range[0]
+                constraint[0], constraint[1] = constraint[1], constraint[0]
+
+        keys_range = np.array([np.nan if x is None else x for x in keys_range])
+
+        positions_limits = self._predict(keys_range)
+        positions_limits = positions_limits.astype(float)
+        positions_limits[np.isnan(keys_range)] = np.nan
+        #print(positions_limits)
+
+        lower = self._range_clarify(keys_range[0], positions_limits[0], True,
+                              constraint[0])
+        upper = self._range_clarify(keys_range[1], positions_limits[1], False,
+                              constraint[1])
+
+        #print(lower, upper)
+
+        return self.data[lower:upper + 1]
+
 
     def is_trained(self):
         return self.trained
@@ -125,8 +205,16 @@ class Lindex:
 
         for attr_name, attr_value in attributes.items():
             if attr_name == "model":
+                #print(attr_name)
                 size += attr_value.size()
-            if attr_name not in ["statistics", "metrics", "history"]:
+            if attr_name not in ["statistics",
+                                 "metrics",
+                                 "history",
+                                 "positions",
+                                 "N",
+                                 "norm_keys",
+                                 "trained"]:
+                #print(attr_name)
                 if isinstance(attr_value, np.ndarray):
                     size += attr_value.nbytes
                 else:
