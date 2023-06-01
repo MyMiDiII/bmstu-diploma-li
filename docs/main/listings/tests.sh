8b5e24a891a2e25f9437ca46512959eba62d2161
MyMiDiII <mymidi.student@yandex.ru>
2023-06-01 09:40:30 +0300

    ADD: testing part and FIX: research headers

---
+++ docs/main/listings/tests.sh
@@ -0,0 +1,19 @@
+======== short test summary info ================
+PASSED tests.py::TestLindex::test_train
+PASSED tests.py::TestLindex::test_middle
+PASSED tests.py::TestLindex::test_first
+PASSED tests.py::TestLindex::test_last
+PASSED tests.py::TestLindex::test_not_exist
+PASSED tests.py::TestLindex::test_range_lw_in
+PASSED tests.py::TestLindex::test_range_lw_out
+PASSED tests.py::TestLindex::test_range_gr_in
+PASSED tests.py::TestLindex::test_range_gr_out
+PASSED tests.py::TestLindex::test_range_le_in
+PASSED tests.py::TestLindex::test_range_le_out
+PASSED tests.py::TestLindex::test_range_ge_in
+PASSED tests.py::TestLindex::test_range_ge_out
+PASSED tests.py::TestLindex::test_range_gle
+PASSED tests.py::TestLindex::test_range_gle_all
+PASSED tests.py::TestLindex::test_range_gle_none
+PASSED tests.py::TestLindex::test_insert
+======= 17 passed, 1 warning in 4.36s ===========
